0dac4309ee40b08e81ca24598f6e261f4e837e95
antoaneta <antoanetapstoyanova@gmail.com>
2025-12-23 16:47:52 +0100

    files

--- src/collect_bluesky_posts.py
+++ src/collect_bluesky_posts.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
-from db.posts_db import save_posts_db
+from db_postgresql.posts_db import save_posts_db
 
 if __name__ == "__main__":
     USERNAME_BLUESKY = os.getenv("USERNAME_BLUESKY")
